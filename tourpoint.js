9bf9d93c5f271ce7fefdedb68c7e002fe3c18bec
Sunattha Lalaeng <158019653+sunattha020@users.noreply.github.com>
2024-05-24 17:48:56 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,217 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "khao Yai National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.50387581091087,
+            14.333546640591237
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palce",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.61839381349148,
+            13.811891363039209
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Icon Siam",
+          "type": "spacial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.51047941534358,
+            13.726828231039178
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Yai Chai Mongkol",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.59257331530353,
+            14.345835769198999
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phra Praton Chedi",
+          "type": "cutural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.06005122883477,
+            13.819868420284337
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bang Khun Tian",
+          "type": "natural",
+          "level": 2
+        },
+        "geometry": {
+          "coordinates": [
+            100.4209150000026,
+            13.555064096033135
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Dream World",
+          "type": "special",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.67487100000005,
+            13.987564635808312
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Pha Ong Floating Market",
+          "type": "special",
+          "level": 2
+        },
+        "geometry": {
+          "coordinates": [
+            100.32306272883619,
+            13.67332563372895
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Amphawa Floating Market",
+          "type": "special",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.95504158465451,
+            13.425648636738856
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Maha But",
+          "type": "cutural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.60711127116207,
+            13.714815507648481
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sarika Waterfall",
+          "type": "natural",
+          "level": 2
+        },
+        "geometry": {
+          "coordinates": [
+            101.25493408465707,
+            14.308956371319368
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bangsaen",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.90161635581859,
+            13.303566024999654
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Phra Samutchedi",
+          "type": "cutural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.58678649999786,
+            13.600362293690196
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Botanical Garden",
+          "type": "natural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.62817557672139,
+            13.553268331616863
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
+
+   
